2308a483b478bf02561c722cd21aedef3719bac6
Divyanshu Singh <79963506+DivyanshuSingh96@users.noreply.github.com>
2022-11-13 12:16:07 +0530

    Delete financeTracker directory

--- financeTracker/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FinancetrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'financeTracker'
--- financeTracker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path("", views.registerPage),
-    path("register/", views.registerPage, name="register"),
-    path("login/", views.loginPage, name="login"),
-    path("", views.home, name="home"),
-]
